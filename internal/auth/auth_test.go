f48d7b32e62b2fea88544f4dfbe5e49ab4d4296e
Mente Gee <dev@imkreative.com>
2025-03-01 15:31:20 -0500

    auth test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,12 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+    t.Run("Test Value", func(t *testing.T) {
+        GetAPIKey(http.Header{})
+    })
+}
